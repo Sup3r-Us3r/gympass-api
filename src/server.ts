83c1456a96819476e3dfc264818ddbf9cef9a9f5
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-09 20:40:56 -0300

    setup .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from './app';
+import { env } from './env';
 
 app
-  .listen({ host: '0.0.0.0', port: 3333 })
+  .listen({ host: '0.0.0.0', port: env.PORT })
   .then(() => console.log('\nHTTP SERVER RUNNING!\n'));
