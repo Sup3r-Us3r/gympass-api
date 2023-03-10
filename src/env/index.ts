8067961342dc71612ae69893705f8377c46da8b6
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-09 21:42:32 -0300

    setup prisma and create user model

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 });
 
