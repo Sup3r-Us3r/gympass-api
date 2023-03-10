7bc82a9d60d76cc15ef823d5ea47e185a7e4e9fa
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-10 11:53:11 -0300

    create in memory database to users repository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,29 @@
+import { Prisma, User } from '@prisma/client';
+
+import { IUsersRepository } from '../users-repository';
+
+class InMemoryUsersRepository implements IUsersRepository {
+  public users: User[] = [];
+
+  async findByEmail(email: string) {
+    const user = this.users.find(user => user.email === email);
+
+    return user ?? null;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+}
+
+export { InMemoryUsersRepository };
