db79891bb7d14c754b2060bd0ff5f84851449393
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-10 20:03:01 -0300

    add create method to in memory gyms repository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,7 +1,8 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 
 interface IGymsRepository {
   findByGymId(gymId: string): Promise<Gym | null>;
+  create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
 
 export { IGymsRepository };
--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,6 @@
-import { Gym } from '@prisma/client';
+import { randomUUID } from 'crypto';
+
+import { Gym, Prisma } from '@prisma/client';
 
 import { IGymsRepository } from '../gyms-repository';
 
@@ -9,6 +11,22 @@ class InMemoryGymsRepository implements IGymsRepository {
     const gym = this.gyms.find(gym => gym.id === gymId);
 
     return gym ?? null;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    };
+
+    this.gyms.push(gym);
+
+    return gym;
   }
 }
 
