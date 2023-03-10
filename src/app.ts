d64d186a83dce33f135d40fb823f29710b1c6842
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-10 10:49:54 -0300

    setup global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 import fastify from 'fastify';
+import { ZodError } from 'zod';
+
+import { env } from './env';
+import { appRoutes } from './http/routes';
 
 const app = fastify();
+
+app.register(appRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' });
+});
 
 export { app };
