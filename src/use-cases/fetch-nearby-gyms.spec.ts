82bc19a303ed6220aab759ca2dc53eef6494fb58
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-11 10:38:21 -0300

    create test for fetch nearby gyms use case

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,64 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
+
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms';
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: FetchNearbyGymsUseCase;
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new FetchNearbyGymsUseCase(gymsRepository);
+  });
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -10.8044212,
+      longitude: -50.5641168,
+    });
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -20.8044212,
+      longitude: -30.5641168,
+    });
+
+    const { gyms } = await sut.execute({
+      userLatitude: -10.80442001,
+      userLongitude: -50.56411001,
+    });
+
+    expect(gyms).toHaveLength(1);
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })]);
+  });
+
+  it.skip('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `gym-${i}`,
+        description: null,
+        phone: null,
+        latitude: -10.8044212,
+        longitude: -50.5641168,
+      });
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'gym',
+      page: 2,
+    });
+
+    expect(gyms).toHaveLength(2);
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'gym-21' }),
+      expect.objectContaining({ title: 'gym-22' }),
+    ]);
+  });
+});
