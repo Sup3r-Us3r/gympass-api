3dcc5f9d19ca8223c1bab86464c2db5f791c6c7e
Sup3r-Us3r <maydersonmello@gmail.com>
2023-03-11 19:38:33 -0300

    create me route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,11 +1,16 @@
 import type { FastifyInstance } from 'fastify';
 
 import { authenticate } from './controllers/authenticate';
+import { profile } from './controllers/profile';
 import { register } from './controllers/register';
+import { verifyJWT } from './middlewares/verify-jwt';
 
 async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  // Authenticated
+  app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
 
 export { appRoutes };
